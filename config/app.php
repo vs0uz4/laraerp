63a9012a06a4e35822f058364807d84e0add4a08
vS0uz4 <vsouza@virtualsystems.com.br>
2015-05-15 11:10:23 -0300

    change app.php config

--- config/app.php
+++ config/app.php
@@ -26,7 +26,7 @@ return [
 	|
 	*/
 
-	'url' => 'http://laraerp.herokuapp.com/',
+	'url' => 'http://laraerp.herokuapp.com',
 
 	/*
 	|--------------------------------------------------------------------------
